0ad2518b2c65de7945aaa4ef4551c49af28dc384
Balaje K <balaje6@gmail.com>
2021-06-21 16:48:33 +1000

    Add separate file for random perturbation

--- Interpolation/interpolate_2.jl
+++ Interpolation/interpolate_2.jl
@@ -23,14 +23,23 @@ f(x) = x[1]^2 + x[2]^2
 fh = interpolate_everywhere(f, V1) # Old FEFunction
 writevtk(Ω1,"source",cellfields=["fh"=>fh])
 
+
 # Second FE Space
-function rndm(p::Point)
+function rndm(p::Point, D=pt_to_rand)
     r, s = p
-    x = r + 0.1*sin(2π*r)*sin(2π*s)
-    y = s + 0.1*sin(2π*r)*sin(2π*s)
+    Random.seed!(get(D,p,1234))
+    x = (r ≈ 1.0 || r ≈ 0.0) ? r : r + 0.012*rand()
+    y = (s ≈ 1.0 || s ≈ 0.0) ? s : s + 0.012*rand()
     Point(x,y)
 end
 partition=(40,40)
+# Construct a uniform partition first to build the perturbation
+model = CartesianDiscreteModel(domain,partition)
+# Random permutation of indices
+pt_to_ind = randperm(num_nodes(model))
+# Build a dictionary with random indices for points
+pt_to_rand = Dict(zip(get_node_coordinates(model),pt_to_ind));
+# Use the dictionary to seed points
 model2 = CartesianDiscreteModel(domain,partition; map=rndm)
 Ω2 = Triangulation(model2)
 reffe = ReferenceFE(lagrangian, Float64, 2)
@@ -39,14 +48,9 @@ V2 = FESpace(model2, reffe)
 # Main Solution
 phys_point = get_cell_points(get_fe_dof_basis(V2)).cell_phys_point
 fh_phys_coords(x) = evaluate(fh, x)
+phys_point_fx = lazy_map(fh_phys_coords, phys_point)
 
 
-gh = CellField( V2, lazy_map(fh_phys_coords, phys_point) )
+gh = CellField( V2, phys_point_fx )
 # Test evaluate on gh (Generic Cell Field): Fails for some points
-@show evaluate(gh, Point(0.1,0.1)) # --Works
-@show evaluate(gh, Point(0.1,0.5)) # --Does not work
-#evaluate(gh, Point(0.1,0.56)) # --Does not work
-# Works sometimes ...
-#for i ∈ 1:10
-#    evaluate(gh, VectorValue(rand(2)))
-#end
+@show evaluate(gh, VectorValue(rand(2))) # Works sometimes ...
