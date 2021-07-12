9c2614bdf5b20d9b27ca5d101f9fe6c907b25882
Balaje K <balaje6@gmail.com>
2021-07-12 23:00:54 +1000

    Working example

--- Interpolation/manifold_test.jl
+++ Interpolation/manifold_test.jl
@@ -11,12 +11,21 @@ using Test
 domain = (0,4,0,4)
 partition = (2,2)
 model = CartesianDiscreteModel(domain,partition)
-btrian = BoundaryTriangulation(model,tags=[5,6,7,8])
-
-#GridTopology(btrian)
-
 labels = get_face_labeling(model)
 bgface_to_mask = get_face_mask(labels,[5,6,7,8],1)
 bmodel = BoundaryDiscreteModel(Polytope{1},model,bgface_to_mask)
 
-GridTopology(bmodel)
+reffe = RaviartThomasRefFE(Float64, QUAD, 0)
+f(x) = VectorValue([x[1], x[2]])
+V1 = FESpace(model, reffe, conformity=:HDiv)
+fh = interpolate_everywhere(f, V1)
+
+# Works if gridtopology.jl is included.
+pt = Point(rand(2))
+@test evaluate(fh, pt) ≈ evaluate(f, pt)
+
+
+
+#GridTopology(btrian)
+
+#GridTopology(bmodel)
